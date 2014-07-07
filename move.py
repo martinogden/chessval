99bf106b0770d297ea94d6b4cf40819f8d254bd1
Martin Ogden <martin.ogden@gmail.com>
2014-07-08 00:51:01 +0100

    correct flag numbers

--- move.py
+++ move.py
@@ -1,10 +1,10 @@
 class flags(object):
-	DPUSH = 0x01
-	KCASTLE = 0x02
-	QCASTLE = 0x03
-	CAPTURE = 0x04
-	EP = 0x05
+    DPUSH = 0x01
+    KCASTLE = 0x02
+    QCASTLE = 0x04
+    CAPTURE = 0x08
+    EP = 0x10
 
 
 def new(frm, to, cpiece=-1, flags=0x00):
-	return (frm, to, cpiece, flags)
+    return (frm, to, cpiece, flags)
